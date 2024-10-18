7dcc5a0c46dea9647c29d62b2e7056cedfacf7f4
krmiller678 <krmiller678@gmail.com>
2024-10-17 23:22:02 -0400

    Added in the CitiesOfInterest.h file and began describing the functionality of that class

---
+++ CitiesOfInterest.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <map>
+#include <string>
+#include "City.h"
+
+/*
+The Cities of Interest class will contain a std::map of sorted "City" strings and their associated City objects.
+This class will recognize the csv being input, and parse the data to pass to the corresponding City.
+If the City does not exist in the map, create new city.
+If the City does exist, call City.addNewDataPoint()
+
+In addition to the above setters, this class will receive the requests from the main file to display the outputs.
+*/
+class CitiesOfInterest
+{
+
+private:
+    std::map<string, City> cities;
+
+
+public:
+    // Constructor
+
+    // Destructor
+
+    // Mutators (Setters)
+        // READ CSV and pass data to City
+
+    // Accessors (Getters)
+
+    // Additional Functions
+
+    // Operator Overloads - NOT USED
+
+};
